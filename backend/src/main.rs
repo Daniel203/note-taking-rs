91ee2a40146a43ed8b688f9421eb7fcb3688a5b4
Daniel Arduini <danielarduini47@gmail.com>
2023-07-27 18:01:19 +0200

    feat: frontend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -38,7 +38,7 @@ async fn main() -> Result<(), sqlx::Error> {
                     .configure(api::notes::service_config),
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8081))?
     .run()
     .await?;
 
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,21 @@
+use leptos::*;
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+
+    view! { cx,
+        <button
+            on:click=move |_| {
+                set_count.set(3);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    mount_to_body(|cx| view! {cx, <App/>})
 }
